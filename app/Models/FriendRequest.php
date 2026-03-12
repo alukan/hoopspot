e6800a2cd3cd8cf651a370e61e9852103cb52cf2
alukan <64636633+alukan@users.noreply.github.com>
2026-03-12 17:18:07 +0700

    add friend requests

---
+++ app/Models/FriendRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class FriendRequest extends Model
+{
+    protected $fillable = [
+        'inviter_id',
+        'invitee_id',
+        'status',
+    ];
+
+    public function inviter(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'inviter_id');
+    }
+
+    public function invitee(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'invitee_id');
+    }
+}
