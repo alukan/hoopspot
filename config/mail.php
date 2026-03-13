5a0db2ea53821cd1b38daa670ebe8651323b4f5f
alukan <64636633+alukan@users.noreply.github.com>
2026-03-13 10:06:27 +0700

    add mail configuration

--- config/mail.php
+++ config/mail.php
@@ -6,6 +6,16 @@ return [
 
     'mailers' => [
 
+        'smtp' => [
+            'transport' => 'smtp',
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+            'port' => env('MAIL_PORT', 587),
+            'scheme' => env('MAIL_SCHEME', 'tls'),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
+            'timeout' => null,
+        ],
+
         'log' => [
             'transport' => 'log',
             'channel' => env('MAIL_LOG_CHANNEL'),
@@ -14,7 +24,7 @@ return [
     ],
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_USERNAME'),
         'name' => env('MAIL_FROM_NAME', env('APP_NAME', 'Laravel')),
     ],
 
